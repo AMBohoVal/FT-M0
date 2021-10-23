29c126be2c6ca4836b6a404b01800af422c79bdc
AMBohoVal <auribohorquez@gmail.com>
2021-10-23 09:50:18 -0500

    Se guardan los archivos de las tareas de la semana

---
+++ 02-JS-I/homework/Tarea.js
@@ -0,0 +1,42 @@
+// Ejercicio que funciono en test, no muestra el return
+// function tienenMismaLongitud(str1, str2) {
+//     var str3 = str1.length;
+//     var str4 = str2.length;
+//     console.log (str3);
+//     console.log (str4);
+
+//      if (str3 === str4) {
+//         return true;
+//     }
+//         else {
+//             return false;
+//         }
+//   }
+
+// // tienenMismaLongitud ("abcd" , "efgh");  
+
+
+// var tex = "Hola";
+// console.log(tex + "!");
+
+// function esEntero(numero) {
+//     // Devuelve "true" si "numero" es un entero (int/integer)
+//     // Ejemplo: 0.8 -> false
+//     // Ejemplo: 1 -> true
+//     // Ejemplo: -10 -> true
+//     // De lo contrario, devuelve "false"
+//     // Pista: Puedes resolver esto usando `Math.floor`
+//     // Tu código:
+//     // if ((numero % 3) === 0 && (numero % 5) === 0) return "fizzbuzz";
+  
+//     var a = numero.length;
+//     console.log (numero);
+
+// }
+
+//   esEntero (156);
+
+var numero = 123;
+
+return numero.length;
+
